8384e18e2b5521b33407a18f3e965b2bc787cf11
Vivek Kr. Dixit <63517775+vivekdixit3911@users.noreply.github.com>
2025-01-01 22:12:43 +0530

    Update About.jsx

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -15,8 +15,8 @@ const About = () => {
           </div>
           <div className='about-text'>
             <h2  className='about-title fs-26 ls-1'>About BookBrowser</h2>
-            <p className='fs-17'>abc</p>
-            <p className='fs-17'>def</p>
+            <p className='fs-17'>The BookBrowser App is designed to provide an intuitive and user-friendly experience for book enthusiasts. Here's how you can use the app effectively</p>
+            <p className='fs-17'>The BookBrowser App is perfect for readers, researchers, and anyone looking to explore the literary world with ease. Start your literary journey today!</p>
           </div>
         </div>
       </div>
